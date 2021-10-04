9665fe163630008e327518c9a05345a202132b37
khaled <khaled.ragab.999@gmail.com>
2021-10-04 04:38:19 +0200

    Bdran commit

---
+++ src/gitjava/Bdran.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gitjava;
+
+/**
+ *
+ * @author KHALED RAGAB
+ */
+public class Bdran {
+    
+}
